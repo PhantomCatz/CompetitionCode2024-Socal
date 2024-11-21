0511ab0fc0ce5b0b9d78b67cc78d3832d9177d30
Kynam L <115427320+Noto501@users.noreply.github.com>
2024-11-20 18:33:51 -0800

    made TurnStraight path slower

--- Crecendo-2024-Refurbished/src/main/java/frc/robot/BuildConstants.java
+++ Crecendo-2024-Refurbished/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crecendo-2024-Refurbished";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 121;
-  public static final String GIT_SHA = "f62276b6aaba57efe3ebcdbfe77d06efa89a508f";
-  public static final String GIT_DATE = "2024-11-20 19:26:35 EST";
+  public static final int GIT_REVISION = 122;
+  public static final String GIT_SHA = "163f7ca4b83e61f486c4260cb73232d316f9ae4f";
+  public static final String GIT_DATE = "2024-11-20 21:20:51 EST";
   public static final String GIT_BRANCH = "Bugfix-After-Questionare-additions";
-  public static final String BUILD_DATE = "2024-11-20 21:14:55 EST";
-  public static final long BUILD_UNIX_TIME = 1732155295942L;
+  public static final String BUILD_DATE = "2024-11-20 21:30:33 EST";
+  public static final long BUILD_UNIX_TIME = 1732156233638L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
